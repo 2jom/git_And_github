7297c0045c2f60a9bc9d02de380d28757c9d40ba
zhou <18256125787@qq.com>
2022-04-18 16:09:23 +0800

    branch1 commit second

--- src/main/java/com/zhou/TestGit.java
+++ src/main/java/com/zhou/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("test2");
+        System.out.println("branch1");
 
     }
 }
